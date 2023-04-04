8c9f11cf171220f9b35f6d1613a87595cc2d055c
CarlosEduardoC1 <cnobre.trabalho@gmail.com>
2023-04-04 14:09:04 -0300

    post route

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,15 @@ app.get("/api/v1/ping", () => {
   return new Date();
 });
 
+app.post("/api/v1/ping", (request, reply) => {
+  // console.log("CONUMED");
+  console.log(request.body);
+  return reply.status(200).send();
+});
+
 app
-  .listen({ 
-    host:"0.0.0.0",
-    port: process.env.PORT ? Number(process.env.PORT) : 3000 })
+  .listen({
+    host: "0.0.0.0",
+    port: process.env.PORT ? Number(process.env.PORT) : 3000,
+  })
   .then(() => console.log("SERVER RUNNING"));
