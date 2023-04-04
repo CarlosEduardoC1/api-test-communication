e4bf8b1e4c0a52d7a0f966d6a8373c15eeed9598
CarlosEduardoC1 <cnobre.trabalho@gmail.com>
2023-04-04 10:30:41 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+app.get("/api/v1/ping", () => {
+  return new Date();
+});
+
+app
+  .listen({ 
+    host:"0.0.0.0",
+    port: process.env.PORT ? Number(process.env.PORT) : 3000 })
+  .then(() => console.log("SERVER RUNNING"));
